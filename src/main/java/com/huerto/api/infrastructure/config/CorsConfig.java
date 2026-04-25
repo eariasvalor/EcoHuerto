7c9df064857f779e99eb3c890ebaecef4aede360
eariasvalor <eariasvalor@gmail.com>
2026-04-25 21:34:43 +0200

    config: add allowed origin for www.ecohuertomontgo.com

--- src/main/java/com/huerto/api/infrastructure/config/CorsConfig.java
+++ src/main/java/com/huerto/api/infrastructure/config/CorsConfig.java
@@ -15,6 +15,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedOrigin("http://localhost:4200");
         config.addAllowedOrigin("https://eariasvalor.github.io");
+        config.addAllowedOrigin("https://ecohuertomontgo.com");
         config.addAllowedOrigin("https://www.ecohuertomontgo.com");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
